5501f787bb86b609b03fa7509db5e1f1b4e48132
GankhulugD <gankhulug12345@gmail.com>
2026-04-08 19:44:05 +0800

    1

--- src/router/order/order.route.ts
+++ src/router/order/order.route.ts
@@ -1,8 +1,12 @@
 import { App } from "../../types";
+import { authMiddleware } from "../../lib/auth-middleware";
 import { createOrder } from "../../controller/orders/post-order";
 import { getOrders } from "../../controller/orders/get-order";
+import { patchOrder } from "../../controller/orders/patch-order";
 
 export const orderRouter = (app: App) => {
-  app.post("/orders", createOrder);
+  app.post("/orders", authMiddleware, createOrder);
+  app.get("/orders", authMiddleware, getOrders);
   app.get("/orders/:userId", getOrders);
+  app.patch("/orders/:orderId", patchOrder);
 };
